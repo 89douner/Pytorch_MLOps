db97b9fbbf7611be5d837efa69e7fa0e9c896c92
PEBpung <mcuwwa29@gmail.com>
2021-10-28 13:59:51 +0900

    [Delete] 255 divide

--- dataload_with_alb.py
+++ dataload_with_alb.py
@@ -1,7 +1,8 @@
 import torch
+from torch._C import device
 from torchvision import transforms
 
-
+import time
 import numpy as np
 from PIL import Image
 import os
@@ -18,7 +19,9 @@ class DiseaseDataset(object):
         self.mode = mode
         self.bit = bit #bit(color) depth
         self.numclasses = num_classes
+        self.imgs = []
         
+        ########################## 전처리 코드 ##########################
         if self.mode == 'train':
             self.transforms = A.Compose([
                 A.Resize(self.image_size, self.image_size),
@@ -40,26 +43,23 @@ class DiseaseDataset(object):
                 A.Normalize(mean=0.5, std=0.5),
                 transforms.ToTensorV2()
             ])
+        ##########################전처리 코드 끝############################
 
         ###data_dir -> train or val 폴더
         self.lst_data = os.listdir(self.data_dir) #train or val 폴더에 들어있는 하위 폴더 -> 클래스 명(알파벳순으로 인덱스 부여) -> ex) normal, pneumonia → lst_data[0]='normal'
         a = len(self.lst_data)
 
         ###data_dir -> train or val // train or val 데이터셋 파일 명과 레이블을 tuple 형태로 구성
-        for i in range(0,a):
-            if i == 0:
-                self.imgs = self.imgs = [(self.lst_data[i], j) for j in list(sorted(os.listdir(os.path.join(self.data_dir, self.lst_data[i]))))]
-            else:
-                self.imgs += [(self.lst_data[i], j) for j  in list(sorted(os.listdir(os.path.join(self.data_dir, self.lst_data[i]))))]
-
+        for i in range(0, a):
+            lst_data_file_name = list(sorted(os.listdir(os.path.join(self.data_dir, self.lst_data[i]))))
+            self.imgs.extend([(self.lst_data[i], j) for j in lst_data_file_name])
         
     def __len__(self):
         return len(self.imgs)
 
-
     def __getitem__(self, idx):
-
-        img_path = os.path.join(self.data_dir, self.imgs[idx][0], self.imgs[idx][1]) #← train or val dataset path + 폴더명(=클래스명) + 파일명 
+        #train or val dataset path + 폴더명(=클래스명) + 파일명
+        img_path = os.path.join(self.data_dir, self.imgs[idx][0], self.imgs[idx][1])  
         
         if self.type == 'img':
             img_data = Image.open(img_path)
@@ -68,24 +68,22 @@ class DiseaseDataset(object):
             if img_data.mode == 'RGB':
                  img_data = img_data.convert('L')
             
-            ##########################전처리 코드 시작##########################
-            np_img = np.array(img_data)/255.0 #pixel 값을 0~1 사이의 값으로 표준화
-            #만약 dicom bit depth가 0~255가 아니면 255 외 다른 값으로 설정해주는 코드를 추가해야함
-            ##########################전처리 코드 끝############################
-
+            # 이미 Albumentation에 ToTensorV2가 있기 때문에 255로 나눌 필요가 없음
+            np_img = np.array(img_data)
+            
             #gray scale인 경우 np_img의 차원이 width, height 2차원으로만 구성 → dimension 차원이 생략됨
             #딥러닝 모델의 모든 입력 값은 dimension 차원을 포함한 3차원으로 구성되어야 함           
             if np_img.ndim == 2:
                 np_img = np_img[:, :, np.newaxis]
             #↑↑↑그래서 np.newaxis로 차원 하나를 더 만듦  (width, height) → (width, height, dimension)
 
-            
             for i in range(0, self.numclasses):
                 if self.imgs[idx][0] == self.lst_data[i]:
                     label = i 
             
             #label = 0 if self.imgs[idx][0] == 'NORMAL' else 1
             img = self.transforms(image=np_img)["image"]
+            
         
         elif type == 'numpy':
             numpy_data = np.load(os.path.join(self.data_dir, self.imgs[idx][0], self.imgs[idx][1]))
@@ -98,14 +96,27 @@ class DiseaseDataset(object):
         
         return img, label
 
-# For test
+
 if __name__ == '__main__':
     data_dir = os.path.join(os.getcwd(), "RSNA_COVID_png_512") #train, val 폴더가 들어있는 경로
     num_classes =  len(os.listdir(os.path.join(data_dir, 'train')))
+    classes_name = os.listdir(os.path.join(data_dir, 'train'))
 
     train_data_dir = os.path.join(os.getcwd(), "RSNA_COVID_png_512", "train")
-    train_dataset = DiseaseDataset(train_data_dir, 512, 8, num_classes, 'img', 'train')
-    dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=1, num_workers=0)
+    train_dataset = DiseaseDataset(train_data_dir, 512, 8, num_classes, classes_name, 'img', 'train')
+    dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, num_workers=0)
 
+    #For shape test
     for imgs, labels in iter(dataloader):
         print(imgs.shape, labels.shape)
+
+    # For time test
+    # device = 'cuda'
+    # iteration = 10
+    # for _ in range(iteration):
+    #     since = time.time()
+    #     for imgs, labels in iter(dataloader):
+    #         torch.nn.Conv2d(1, 3, (3, 3), device=device)(imgs.to(device))
+    
+    # time_elapsed = time.time() - since
+    # print(time_elapsed)
--- sweep_classification.py
+++ sweep_classification.py
@@ -88,8 +88,8 @@ def wandb_setting():
     #model_ft = sweep_train.train_model(dataloaders, dataset_sizes, num_iteration, net, criterion, optimizer_ft, scheduler_warmup,  device, wandb, num_epoch=30)
 
 #sweep_id = wandb.sweep(config.sweep_config, project="test", entity="douner89")
-#sweep_id = wandb.sweep(config.sweep_config, project="rsna_covid", entity="pneumonia")
-sweep_id = wandb.sweep(config.sweep_config, project="rsna_covid", entity="89douner")
+sweep_id = wandb.sweep(config.sweep_config, project="rsna_covid", entity="pneumonia")
+#sweep_id = wandb.sweep(config.sweep_config, project="rsna_covid", entity="89douner")
 
 wandb.agent(sweep_id, wandb_setting, count=48)
 
